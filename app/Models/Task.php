407be1a5f8189cfea0cce9364ec4e869c4997119
Basma Salem <basmahhsalem@gmail.com>
2025-07-19 23:54:38 +0300

    update task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,6 +12,7 @@ use Laravel\Passport\HasApiTokens;
 class Task extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $primaryKey = 'task_id'; 
     /**
      * The attributes that are mass assignable.
      *
@@ -28,23 +29,5 @@ class Task extends Authenticatable
         'updated_by',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    // protected $hidden = [
-    //     'password',
-    //     'remember_token',
-    // ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    
 }
